ce4fea3002b6b49bbb08cf91225678713a8a157c
chrissiku <chrissiku5@gmail.com>
2022-06-16 15:01:01 +0200

    Add function to postData to the GAME

--- src/api.js
+++ src/api.js
@@ -1,7 +1,26 @@
 /* eslint-disable quotes */
 import scoreElement from "./display.js";
 
-const url = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/";
+const apiRouteKey = "games/Zl4d7IVkemOTTVg2fUdz/scores/";
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/${apiRouteKey}`;
+
+const postData = async (form) => {
+  const formData = new FormData(form);
+  const formDataSer = Object.fromEntries(formData);
+  const myJsonObject = { ...formDataSer, score: Number(formDataSer.score) };
+
+  try {
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(myJsonObject),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (e) {
+    throw new Error(e);
+  }
+};
 
 const getData = async () => {
   const response = await fetch(url, { mode: "cors" });
@@ -20,4 +39,4 @@ const displayData = () => {
   });
 };
 
-export default displayData;
+export { postData, displayData };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import "./style.css";
 import { add, myList, Scores } from "./add.js";
 import scoreElement from "./display.js";
-import displayData from "./api.js";
+import { postData, displayData } from "./api.js";
 
 const storage = JSON.parse(localStorage.getItem("scores")) || [];
 const addBtn = document.querySelector("#submit");
@@ -10,8 +10,7 @@ const nameField = document.querySelector("#name");
 const scoreField = document.querySelector("#score");
 const refreshBtn = document.querySelector("#refresh");
 const container = document.querySelector(".scores");
-
-displayData();
+const form = document.querySelector("form");
 
 storage.forEach((element) => {
   const myScoreData = new Scores(element.name, element.score);
@@ -19,12 +18,22 @@ storage.forEach((element) => {
   scoreElement(myScoreData.name, myScoreData.score);
 });
 
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  postData(form);
+  form.children[1].value = "";
+  form.children[2].value = "";
+});
+
 addBtn.addEventListener("click", () => {
   add(nameField.value, scoreField.value);
 });
 
 refreshBtn.addEventListener("click", () => {
-  // document.location.href = "./index.html";
-  container.innerHTML = '';
+  container.innerHTML = "";
+  displayData();
+});
+
+window.addEventListener("DOMContentLoaded", () => {
   displayData();
 });
