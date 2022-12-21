9f81f6b5a2e419d9f9fce29a8b55c50b0dd51c26
Chris Siku <chrissiku5@gmail.com>
2022-12-21 11:02:32 +0200

    refactor function to add new score

--- src/index.js
+++ src/index.js
@@ -2,26 +2,44 @@
 /* eslint-disable operator-linebreak */
 /* eslint-disable quotes */
 
+const baseUrl =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/";
 const gameId = "SsPx20xTXWNtqFtQ6FtC";
 
+const scoreUrl = `${baseUrl + gameId}/scores`;
+
 const createGame = async () => {
-  await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        name: "Christian Siku Game",
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  )
+  await fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: "Christian Siku Game",
+    }),
+  })
     .then((response) => response.json())
     .then((json) => {
       json;
       console.log(json);
     });
 };
-
 // createGame();
+
+const addScore = async (user, score) => {
+  await fetch(scoreUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user,
+      score,
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      json;
+    })
+    .catch((error) => error);
+};
