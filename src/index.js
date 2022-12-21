5280cefdec825d1fc3b5857a8a9f977d34cdecae
Chris Siku <chrissiku5@gmail.com>
2022-12-21 12:52:31 +0200

    Refactor full code

--- src/index.js
+++ src/index.js
@@ -46,12 +46,12 @@ const addScore = async (user, score) => {
 const singleRow = (players) => {
   let boardRow = "";
   if (players.length === 0) {
-    boardRow += `<tr><td id="winner">0</td><td><img src="./images/User1.jpg"><p>No Player</p></td><td>0</td></tr>`;
+    boardRow += `<tr><td id="winner">0</td><td class="player"><img src="./images/User1.jpg"><p>No Player</p></td><td>0</td></tr>`;
   } else {
     players.forEach((player, index) => {
       boardRow += `<tr><td id="winner">${
         index + 1
-      }</td><td><img src="./images/User1.jpg"><p>${player.user}</p></td><td>${
+      }</td><td class="player"><img src="./images/User1.jpg"><p>${player.user}</p></td><td>${
         player.score
       }</td></tr>`;
     });
@@ -71,10 +71,19 @@ const playerName = document.querySelector("#user");
 const playerScore = document.querySelector("#score");
 const refresh = document.querySelector(".refresh");
 
+// Save form
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   addScore(playerName.value, playerScore.value);
-  document.querySelector(".form").reset();
+  window.location.reload();
 });
 
-display();
+// refresh feature
+refresh.addEventListener("click", () => {
+  window.location.reload();
+});
+
+setTimeout(function () {
+  display();
+}, 1000);
